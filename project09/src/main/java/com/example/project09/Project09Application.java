53e5848733dc95c97aef455b0f75d466ad0277d8
김범진 <sdpthf@naver.com>
2021-08-26 22:50:07 +0900

    [ADD] Project09Application

---
+++ project09/src/main/java/com/example/project09/Project09Application.java
@@ -0,0 +1,13 @@
+package com.example.project09;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Project09Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Project09Application.class, args);
+    }
+
+}
