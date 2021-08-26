8c23e4402c39a5884288c252aa776b2c209a01af
김범진 <sdpthf@naver.com>
2021-08-26 22:50:52 +0900

    [ADD] Project09ApplicationTest

---
+++ project09/src/test/java/com/example/project09/Project09ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.project09;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Project09ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
