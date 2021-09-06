cbd666cc75d55ed51951d5454ea6ec6af3298f58
김범진 <sdpthf@naver.com>
2021-09-06 15:56:53 +0900

    [UPDATE] LikeRepository

--- project09/src/main/java/com/example/project09/entity/like/LikeRepository.java
+++ project09/src/main/java/com/example/project09/entity/like/LikeRepository.java
@@ -1,4 +1,6 @@
 package com.example.project09.entity.like;
 
-public interface LikeRepository {
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface LikeRepository extends JpaRepository<Like, Integer> {
 }
