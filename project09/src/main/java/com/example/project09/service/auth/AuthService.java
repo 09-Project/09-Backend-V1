6d77deb503f328281d74246e73bb6184ee378fef
김범진 <sdpthf@naver.com>
2021-09-06 15:50:35 +0900

    [UPDATE] AuthService

--- project09/src/main/java/com/example/project09/service/auth/AuthService.java
+++ project09/src/main/java/com/example/project09/service/auth/AuthService.java
@@ -1,2 +1,10 @@
-package com.example.project09.service.auth;public interface AuthService {
+package com.example.project09.service.auth;
+
+import com.example.project09.payload.auth.request.LoginRequest;
+import com.example.project09.payload.auth.request.SignupRequest;
+import com.example.project09.payload.auth.response.AccessTokenResponse;
+
+public interface AuthService {
+    String signup(SignupRequest request);
+    AccessTokenResponse login(LoginRequest request);
 }
--- project09/src/main/java/com/example/project09/service/auth/AuthServiceImpl.java
+++ project09/src/main/java/com/example/project09/service/auth/AuthServiceImpl.java
@@ -1,2 +1,51 @@
-package com.example.project09.service.auth;public class AuthServiceImpl {
+package com.example.project09.service.auth;
+
+import com.example.project09.entity.member.Member;
+import com.example.project09.entity.member.MemberRepository;
+import com.example.project09.payload.auth.request.LoginRequest;
+import com.example.project09.payload.auth.request.SignupRequest;
+import com.example.project09.payload.auth.response.AccessTokenResponse;
+import com.example.project09.security.jwt.JwtTokenProvider;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class AuthServiceImpl implements AuthService {
+
+    private final MemberRepository memberRepository;
+    private final JwtTokenProvider tokenProvider;
+    private final PasswordEncoder passwordEncoder;
+
+    @Override
+    public String signup(SignupRequest request) {
+        if(memberRepository.existsByNameOrUsername(request.getName(), request.getUsername())) {
+            throw new IllegalArgumentException();
+        }
+
+        Member member = Member.builder()
+                .name(request.getName())
+                .username(request.getUsername())
+                .password(passwordEncoder.encode(request.getPassword()))
+                .introduction(request.getIntroduction())
+                .build();
+
+        memberRepository.save(member);
+
+        return "success signup";
+    }
+
+    @Override
+    public AccessTokenResponse login(LoginRequest request) {
+        Member member = memberRepository.findByUsername(request.getUsername())
+                .orElseThrow(() -> new IllegalArgumentException());
+
+        if(!passwordEncoder.matches(request.getPassword(), member.getPassword())) {
+            throw new IllegalArgumentException();
+        }
+
+        return tokenProvider.createAccessToken(member.getUsername());
+    }
+
 }
