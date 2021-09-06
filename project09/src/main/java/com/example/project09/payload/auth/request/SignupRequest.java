cf882e13ae3866eb1612b5d8c70773e579116f8f
김범진 <sdpthf@naver.com>
2021-09-06 10:18:00 +0900

    [ADD] SignupRequest

---
+++ project09/src/main/java/com/example/project09/payload/auth/request/SignupRequest.java
@@ -0,0 +1,14 @@
+package com.example.project09.payload.auth.request;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+public class SignupRequest {
+    private String name;
+    private String username;
+    private String password;
+}
