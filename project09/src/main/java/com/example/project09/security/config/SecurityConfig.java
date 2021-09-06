5e7532f7350a317b14cbf605080606c7416f186d
김범진 <sdpthf@naver.com>
2021-09-06 15:52:20 +0900

    [UPDATE] Add JwtFilter at SecurityConfig

--- project09/src/main/java/com/example/project09/security/config/SecurityConfig.java
+++ project09/src/main/java/com/example/project09/security/config/SecurityConfig.java
@@ -1,5 +1,7 @@
 package com.example.project09.security.config;
 
+import com.example.project09.security.jwt.JwtTokenProvider;
+import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -9,7 +11,10 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
 @EnableWebSecurity
+@RequiredArgsConstructor
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    private final JwtTokenProvider tokenProvider;
 
     @Bean
     public PasswordEncoder passwordEncoder() {
@@ -29,7 +34,10 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                 .and()
                 .authorizeRequests()
                 .antMatchers("/auth/**").permitAll()
-                .anyRequest().authenticated();
+                .anyRequest().authenticated()
+
+                .and()
+                .apply(new FilterConfig(tokenProvider));
     }
 
 }
