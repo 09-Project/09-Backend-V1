50973e0b425d1b031a391dd64e06fd771f586aca
김범진 <sdpthf@naver.com>
2021-09-06 15:49:36 +0900

    [UPDATE] Add SignupController

--- project09/src/main/java/com/example/project09/controller/AuthController.java
+++ project09/src/main/java/com/example/project09/controller/AuthController.java
@@ -1,4 +1,23 @@
 package com.example.project09.controller;
 
+import com.example.project09.payload.auth.request.SignupRequest;
+import com.example.project09.service.auth.AuthServiceImpl;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/auth")
 public class AuthController {
+
+    private AuthServiceImpl authService;
+
+    @PostMapping("/signup")
+    public String signup(@RequestBody SignupRequest request) {
+        return authService.signup(request);
+    }
+
 }
