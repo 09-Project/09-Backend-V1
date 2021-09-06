b5e7f49a72a2589dff1d61829c085016a776c4d5
김범진 <sdpthf@naver.com>
2021-09-06 15:54:14 +0900

    [UPDATE] Member Entity

--- project09/src/main/java/com/example/project09/entity/member/Member.java
+++ project09/src/main/java/com/example/project09/entity/member/Member.java
@@ -1,4 +1,38 @@
 package com.example.project09.entity.member;
 
+import com.example.project09.entity.image.Image;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import javax.persistence.*;
+import javax.validation.constraints.Max;
+
+@Getter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
 public class Member {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "member_id")
+    private Integer id;
+
+    @Max(value = 10)
+    @Column(unique = true)
+    private String name;
+
+    @Column(unique = true)
+    private String username;
+    private String password;
+
+    @Max(value = 200)
+    private String introduction;
+
+    @OneToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "imageId")
+    private Image image;
+
 }
