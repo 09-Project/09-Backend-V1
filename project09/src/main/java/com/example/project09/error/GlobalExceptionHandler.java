dec3ca79617a2e6e22edbf97ca18689dfc019adb
김범진 <sdpthf@naver.com>
2021-09-07 08:40:41 +0900

    [ADD] GlobalExceptionHandler

---
+++ project09/src/main/java/com/example/project09/error/GlobalExceptionHandler.java
@@ -0,0 +1,19 @@
+package com.example.project09.error;
+
+import com.example.project09.error.exception.BusinessException;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler(BusinessException.class)
+    public ResponseEntity<ErrorResponse> handleException(BusinessException e) {
+        final ErrorCode errorCode = e.getErrorCode();
+        return new ResponseEntity<>(new ErrorResponse(errorCode.getStatus(), errorCode.getMessage()),
+                HttpStatus.valueOf(errorCode.getStatus()));
+    }
+
+}
