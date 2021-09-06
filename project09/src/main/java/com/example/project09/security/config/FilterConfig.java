e97636d201082c517a2902d5dde116a3f7ca7fcb
김범진 <sdpthf@naver.com>
2021-09-06 10:36:03 +0900

    [UPDATE] Add JwtTokenFilter Before UsernamePasswordAuthenticationFilter

--- project09/src/main/java/com/example/project09/security/config/FilterConfig.java
+++ project09/src/main/java/com/example/project09/security/config/FilterConfig.java
@@ -1,4 +1,22 @@
 package com.example.project09.security.config;
 
-public class FilterConfig {
+import com.example.project09.security.jwt.JwtTokenFilter;
+import com.example.project09.security.jwt.JwtTokenProvider;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.config.annotation.SecurityConfigurerAdapter;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.DefaultSecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+@RequiredArgsConstructor
+public class FilterConfig extends SecurityConfigurerAdapter<DefaultSecurityFilterChain, HttpSecurity> {
+
+    private final JwtTokenProvider tokenProvider;
+
+    @Override
+    public void configure(HttpSecurity builder) throws Exception {
+        JwtTokenFilter filter = new JwtTokenFilter(tokenProvider);
+        builder.addFilterBefore(filter, UsernamePasswordAuthenticationFilter.class);
+    }
+    
 }
