c9820beffd4f841313386bc8623c86dfb0ef8180
김범진 <sdpthf@naver.com>
2021-09-06 15:56:41 +0900

    [UPDATE] Like Entity

--- project09/src/main/java/com/example/project09/entity/like/Like.java
+++ project09/src/main/java/com/example/project09/entity/like/Like.java
@@ -1,4 +1,26 @@
 package com.example.project09.entity.like;
 
+import com.example.project09.entity.post.Post;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import javax.persistence.*;
+
+@Getter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
 public class Like {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "like_id")
+    private Integer id;
+    private Integer count;
+
+    @ManyToOne
+    @JoinColumn(name = "postId")
+    private Post post;
 }
