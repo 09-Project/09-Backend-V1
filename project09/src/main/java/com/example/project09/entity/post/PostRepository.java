b394786c508798baf8643a9fd54153ce267798e7
김범진 <sdpthf@naver.com>
2021-09-06 15:53:48 +0900

    [UPDATE] PostRepository

--- project09/src/main/java/com/example/project09/entity/post/PostRepository.java
+++ project09/src/main/java/com/example/project09/entity/post/PostRepository.java
@@ -1,4 +1,6 @@
 package com.example.project09.entity.post;
 
-public interface PostRepository {
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface PostRepository extends JpaRepository<Post, Integer> {
 }
