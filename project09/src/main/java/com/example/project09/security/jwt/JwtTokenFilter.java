737682e94a33e39a710ceac914360d246eff32f1
김범진 <sdpthf@naver.com>
2021-09-06 15:51:35 +0900

    [UPDATE] JwtTokenFilter

--- project09/src/main/java/com/example/project09/security/jwt/JwtTokenFilter.java
+++ project09/src/main/java/com/example/project09/security/jwt/JwtTokenFilter.java
@@ -1,4 +1,32 @@
 package com.example.project09.security.jwt;
 
-public class JwtTokenFilter {
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.web.filter.OncePerRequestFilter;
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@RequiredArgsConstructor
+public class JwtTokenFilter extends OncePerRequestFilter {
+
+    private final JwtTokenProvider tokenProvider;
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request,
+                                    HttpServletResponse response,
+                                    FilterChain filterChain) throws ServletException, IOException {
+        String token = tokenProvider.resolveToken(request);
+
+        if(token != null && tokenProvider.validateToken(token)) {
+            Authentication authentication = tokenProvider.getAuthentication(token);
+            SecurityContextHolder.getContext().setAuthentication(authentication);
+        }
+
+        filterChain.doFilter(request, response);
+    }
+
 }
