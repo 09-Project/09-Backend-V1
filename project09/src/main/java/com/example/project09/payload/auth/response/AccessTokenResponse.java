10cf840b7d5df2ac45983a55e00fda0dffaa38da
김범진 <sdpthf@naver.com>
2021-09-06 10:18:22 +0900

    [ADD] AccessTokenResponse

---
+++ project09/src/main/java/com/example/project09/payload/auth/response/AccessTokenResponse.java
@@ -0,0 +1,10 @@
+package com.example.project09.payload.auth.response;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public class AccessTokenResponse {
+    private String accessToken;
+}
