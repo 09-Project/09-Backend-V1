08e98b2c5c3d6ac3f427295cc9e4f9511ed6c14d
김범진 <sdpthf@naver.com>
2021-09-06 15:54:33 +0900

    [UPDATE] MemberRepository

--- project09/src/main/java/com/example/project09/entity/member/MemberRepository.java
+++ project09/src/main/java/com/example/project09/entity/member/MemberRepository.java
@@ -1,4 +1,9 @@
 package com.example.project09.entity.member;
 
-public interface MemberRepository {
+import org.springframework.data.jpa.repository.JpaRepository;
+import java.util.Optional;
+
+public interface MemberRepository extends JpaRepository<Member, Integer> {
+    boolean existsByNameOrUsername(String name, String username);
+    Optional<Member> findByUsername(String username);
 }
