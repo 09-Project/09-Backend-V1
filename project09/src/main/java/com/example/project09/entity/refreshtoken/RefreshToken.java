5e6112cfb3516f0c7dbad059e07170af68b4adff
김범진 <sdpthf@naver.com>
2021-09-07 21:45:05 +0900

    [ADD] RefreseToken

---
+++ project09/src/main/java/com/example/project09/entity/refreshtoken/RefreshToken.java
@@ -0,0 +1,33 @@
+package com.example.project09.entity.refreshtoken;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.redis.core.RedisHash;
+import org.springframework.data.redis.core.TimeToLive;
+import org.springframework.data.redis.core.index.Indexed;
+
+@Getter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@RedisHash
+public class RefreshToken {
+
+    @Id
+    private String username;
+
+    @Indexed
+    private String refreshToken;
+
+    @TimeToLive
+    private Long refreshExpiration;
+
+    public RefreshToken update(Long refreshExp) {
+        this.refreshExpiration = refreshExp;
+        return this;
+    }
+
+}
