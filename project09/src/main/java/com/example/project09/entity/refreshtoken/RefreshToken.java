45ec6e70e298226f858e64ec30a5e7aa01a14e3e
김범진 <sdpthf@naver.com>
2021-09-07 23:16:03 +0900

    [UPDATE] Appoint RefreshToken Key

--- project09/src/main/java/com/example/project09/entity/refreshtoken/RefreshToken.java
+++ project09/src/main/java/com/example/project09/entity/refreshtoken/RefreshToken.java
@@ -13,7 +13,7 @@ import org.springframework.data.redis.core.index.Indexed;
 @Builder
 @NoArgsConstructor
 @AllArgsConstructor
-@RedisHash
+@RedisHash(value = "refresh_token")
 public class RefreshToken {
 
     @Id
