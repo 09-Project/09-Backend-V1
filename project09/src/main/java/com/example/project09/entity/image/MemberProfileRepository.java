424f66a6db215279ee809f81d91e9e67e1566cc4
김범진 <sdpthf@naver.com>
2021-09-07 23:12:01 +0900

    [DELETE] MemberProfile

--- project09/src/main/java/com/example/project09/entity/image/MemberProfileRepository.java
+++
@@ -1,6 +0,0 @@
-package com.example.project09.entity.image;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface MemberProfileRepository extends JpaRepository<MemberProfile, Integer> {
-}
