44f0fa714f971218090031a97ce3b86a2c9c8605
김범진 <sdpthf@naver.com>
2021-09-07 12:52:52 +0900

    [UPDATE] toString

--- project09/src/main/java/com/example/project09/error/ErrorResponse.java
+++ project09/src/main/java/com/example/project09/error/ErrorResponse.java
@@ -2,9 +2,12 @@ package com.example.project09.error;
 
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.ToString;
 
 @Getter
+@ToString
 @AllArgsConstructor
 public class ErrorResponse {
+    private final int status;
     private final String message;
 }
