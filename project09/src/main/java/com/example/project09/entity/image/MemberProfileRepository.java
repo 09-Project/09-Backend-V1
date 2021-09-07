e4a3673647de90b206cae44064ff51a82cab7207
김범진 <sdpthf@naver.com>
2021-09-07 15:04:27 +0900

    [ADD] MemberProfileRepository

---
+++ project09/src/main/java/com/example/project09/entity/image/MemberProfileRepository.java
@@ -0,0 +1,6 @@
+package com.example.project09.entity.image;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface MemberProfileRepository extends JpaRepository<MemberProfile, Integer> {
+}
