44e02c56db44c44d68782dd96e2031abf5fe6b8e
김범진 <sdpthf@naver.com>
2021-09-08 23:01:05 +0900

    [UPDATE] Update Information and Password Service

--- project09/src/main/java/com/example/project09/service/member/MemberService.java
+++ project09/src/main/java/com/example/project09/service/member/MemberService.java
@@ -1,2 +1,10 @@
-package com.example.project09.service.member;public interface MemberService {
+package com.example.project09.service.member;
+
+import com.example.project09.entity.member.Member;
+import com.example.project09.payload.member.request.InformationRequest;
+import com.example.project09.payload.member.request.PasswordRequest;
+
+public interface MemberService {
+    void updatePassword(PasswordRequest request, Member member);
+    void updateInfo(InformationRequest request);
 }
--- project09/src/main/java/com/example/project09/service/member/MemberServiceImpl.java
+++ project09/src/main/java/com/example/project09/service/member/MemberServiceImpl.java
@@ -1,4 +1,45 @@
 package com.example.project09.service.member;
 
-public class MemberServiceImpl {
+import com.example.project09.entity.member.Member;
+import com.example.project09.entity.member.MemberRepository;
+import com.example.project09.exception.InvalidPasswordException;
+import com.example.project09.exception.UserNotFoundException;
+import com.example.project09.payload.member.request.InformationRequest;
+import com.example.project09.payload.member.request.PasswordRequest;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class MemberServiceImpl implements MemberService {
+    private final MemberRepository memberRepository;
+    private final PasswordEncoder passwordEncoder;
+
+    @Override
+    public void updatePassword(PasswordRequest request, Member member) {
+        checkPassword(request.getPassword(), member.getUsername());
+        memberRepository.findByUsername(member.getUsername())
+                .map(password -> memberRepository.save(
+                        password.updatePassword(passwordEncoder.encode(request.getNewPassword()))
+                ))
+                .orElseThrow(UserNotFoundException::new);
+    }
+
+    @Override
+    public void updateInfo(InformationRequest request) {
+        memberRepository.findByName(request.getName())
+                .map(info -> memberRepository.save(
+                        info.updateInfo(request.getName(), request.getIntroduction())
+                ))
+                .orElseThrow(UserNotFoundException::new);
+    }
+
+    public void checkPassword(String password, String username) {
+        Member member = memberRepository.findByUsername(username)
+                .orElseThrow(UserNotFoundException::new);
+
+        if(!passwordEncoder.matches(password, member.getPassword())) throw new InvalidPasswordException();
+    }
+
 }
