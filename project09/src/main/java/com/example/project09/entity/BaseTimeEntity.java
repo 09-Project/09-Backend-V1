93fa077cc235f468d96f09969a91ada8dfa7ffff
김범진 <sdpthf@naver.com>
2021-09-08 23:04:37 +0900

    [UPDATE] BaseTimeEntity to Abstract

--- project09/src/main/java/com/example/project09/entity/BaseTimeEntity.java
+++ project09/src/main/java/com/example/project09/entity/BaseTimeEntity.java
@@ -11,7 +11,7 @@ import java.time.LocalDateTime;
 @Getter
 @MappedSuperclass
 @EntityListeners(AuditingEntityListener.class)
-public class BaseTimeEntity {
+public abstract class BaseTimeEntity {
 
     @CreatedDate
     private LocalDateTime createdDate;
