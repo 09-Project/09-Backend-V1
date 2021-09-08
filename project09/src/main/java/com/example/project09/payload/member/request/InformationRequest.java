170a252fab18545bd2d6512f4999b47aa12eb6a6
김범진 <sdpthf@naver.com>
2021-09-08 23:00:33 +0900

    [ADD] InformationRequest

---
+++ project09/src/main/java/com/example/project09/payload/member/request/InformationRequest.java
@@ -0,0 +1,13 @@
+package com.example.project09.payload.member.request;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+public class InformationRequest {
+    private String name;
+    private String introduction;
+}
