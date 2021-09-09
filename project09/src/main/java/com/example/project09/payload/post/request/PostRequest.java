4d57086dfd4f3b1b2c44ffcd0efd467148d36532
김범진 <sdpthf@naver.com>
2021-09-09 15:20:06 +0900

    [UPDATE] MultipartFiles

--- project09/src/main/java/com/example/project09/payload/post/request/PostRequest.java
+++ project09/src/main/java/com/example/project09/payload/post/request/PostRequest.java
@@ -3,14 +3,15 @@ package com.example.project09.payload.post.request;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import org.springframework.web.multipart.MultipartFile;
+import java.util.List;
 
 @Getter
 @AllArgsConstructor
-public class CreatePostRequest {
+public class PostRequest {
     private String title;
     private String content;
     private Integer price;
     private String transactionRegion;
     private String openChatLink;
-    private MultipartFile multipartFile;
+    private List<MultipartFile> multipartFiles;
 }
