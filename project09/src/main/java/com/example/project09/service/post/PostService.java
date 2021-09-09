aeb041af5ad1e4ba0d8a4fb7b6f5f68a74c0845b
김범진 <sdpthf@naver.com>
2021-09-09 13:59:55 +0900

    [UPDATE] Create Post Service

--- project09/src/main/java/com/example/project09/service/post/PostService.java
+++ project09/src/main/java/com/example/project09/service/post/PostService.java
@@ -1,4 +1,8 @@
 package com.example.project09.service.post;
 
+import com.example.project09.entity.member.Member;
+import com.example.project09.payload.post.request.CreatePostRequest;
+
 public interface PostService {
+    void create(CreatePostRequest request, Member member);
 }
--- project09/src/main/java/com/example/project09/service/post/PostServiceImpl.java
+++ project09/src/main/java/com/example/project09/service/post/PostServiceImpl.java
@@ -1,7 +1,37 @@
 package com.example.project09.service.post;
 
+import com.example.project09.entity.image.Image;
+import com.example.project09.entity.image.ImageRepository;
+import com.example.project09.entity.member.Member;
+import com.example.project09.entity.post.Post;
+import com.example.project09.entity.post.PostRepository;
+import com.example.project09.entity.post.Purpose;
+import com.example.project09.payload.post.request.CreatePostRequest;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 @Service
+@RequiredArgsConstructor
 public class PostServiceImpl implements PostService {
+    private final PostRepository postRepository;
+    private final ImageRepository imageRepository;
+
+    @Override
+    public void create(CreatePostRequest request, Member member) {
+        Post post = postRepository.save(Post.builder()
+                .title(request.getTitle())
+                .content(request.getContent())
+                .price(request.getPrice())
+                .transactionRegion(request.getTransactionRegion())
+                .openChatLink(request.getOpenChatLink())
+                .purpose(Purpose.CO_PURCHASE)
+                .member(member)
+                .build());
+
+        imageRepository.save(Image.builder()
+                .profileUrl(request.getMultipartFile().getOriginalFilename())
+                .post(post)
+                .build());
+    }
+
 }
