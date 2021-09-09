206f2cddbd287e25837b70ee49112d6845121b9b
김범진 <sdpthf@naver.com>
2021-09-09 13:59:20 +0900

    [UPDATE] CreatePost Controller

--- project09/src/main/java/com/example/project09/controller/PostController.java
+++ project09/src/main/java/com/example/project09/controller/PostController.java
@@ -1,7 +1,24 @@
 package com.example.project09.controller;
 
-import org.springframework.web.bind.annotation.RestController;
+import com.example.project09.payload.post.request.CreatePostRequest;
+import com.example.project09.security.auth.CustomUserDetails;
+import com.example.project09.service.post.PostServiceImpl;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
+@RequiredArgsConstructor
+@RequestMapping("/post")
 public class PostController {
+    private final PostServiceImpl postService;
+
+    @PostMapping(path = "crate", consumes = {"multipart/form-data"})
+    @ResponseStatus(HttpStatus.CREATED)
+    public void createPost(@ModelAttribute CreatePostRequest request,
+                           @AuthenticationPrincipal CustomUserDetails customUserDetails) {
+        postService.create(request, customUserDetails.getMember());
+    }
+
 }
