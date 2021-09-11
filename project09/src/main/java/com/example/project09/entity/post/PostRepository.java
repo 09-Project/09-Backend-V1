1d3c8b6d3d383c6bb6df89639da8201ad84d5cfd
김범진 <sdpthf@naver.com>
2021-09-11 11:35:23 +0900

    [UPDATE] FindByMemberId

--- project09/src/main/java/com/example/project09/entity/post/PostRepository.java
+++ project09/src/main/java/com/example/project09/entity/post/PostRepository.java
@@ -1,6 +1,8 @@
 package com.example.project09.entity.post;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import java.util.List;
 
 public interface PostRepository extends JpaRepository<Post, Integer> {
+    List<Post> findByMemberId(Integer id);
 }
