7e951a05f9f330a8c835d2f7bfd124e0d3d1b13b
김범진 <sdpthf@naver.com>
2021-09-13 10:33:45 +0900

    [UPDATE] MemberProfile Controller

--- project09/src/main/java/com/example/project09/controller/MemberController.java
+++ project09/src/main/java/com/example/project09/controller/MemberController.java
@@ -1,14 +1,13 @@
 package com.example.project09.controller;
 
-import com.example.project09.payload.member.request.InformationRequest;
-import com.example.project09.payload.member.request.PasswordRequest;
-import com.example.project09.payload.post.response.PostResponse;
+import com.example.project09.payload.member.request.UpdateInformationRequest;
+import com.example.project09.payload.member.request.UpdatePasswordRequest;
+import com.example.project09.payload.member.response.MemberProfileResponse;
 import com.example.project09.security.auth.CustomUserDetails;
 import com.example.project09.service.member.MemberServiceImpl;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.*;
-import java.util.List;
 
 @RestController
 @RequiredArgsConstructor
@@ -17,20 +16,20 @@ public class MemberController {
     private final MemberServiceImpl memberService;
 
     @PatchMapping("/password")
-    public void updatePassword(@RequestBody PasswordRequest request,
+    public void updatePassword(@RequestBody UpdatePasswordRequest request,
                                @AuthenticationPrincipal CustomUserDetails userDetails) {
         memberService.updatePassword(request, userDetails.getMember());
     }
 
     @PatchMapping(path = "/information", consumes = {"multipart/form-data"})
-    public void updateInfo(@ModelAttribute InformationRequest request,
+    public void updateInfo(@ModelAttribute UpdateInformationRequest request,
                            @AuthenticationPrincipal CustomUserDetails userDetails) {
         memberService.updateInfo(request, userDetails.getMember());
     }
 
-    @GetMapping("/posts")
-    public List<PostResponse> getMemberPosts(@AuthenticationPrincipal CustomUserDetails userDetails) {
-        return memberService.getMemberPosts(userDetails.getMember());
+    @GetMapping("/profile/{user-id}")
+    public MemberProfileResponse getMemberProfile(@PathVariable(name = "user-id") Integer id) {
+        return memberService.getMemberProfile(id);
     }
 
 }
