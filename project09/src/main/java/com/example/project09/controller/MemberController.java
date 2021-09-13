03cd5bee8985e6c56422272f4f1d1be033a3db69
김범진 <sdpthf@naver.com>
2021-09-13 09:39:09 +0900

    [UPDATE] Get Member Posts

--- project09/src/main/java/com/example/project09/controller/MemberController.java
+++ project09/src/main/java/com/example/project09/controller/MemberController.java
@@ -2,14 +2,13 @@ package com.example.project09.controller;
 
 import com.example.project09.payload.member.request.InformationRequest;
 import com.example.project09.payload.member.request.PasswordRequest;
+import com.example.project09.payload.post.response.PostResponse;
 import com.example.project09.security.auth.CustomUserDetails;
 import com.example.project09.service.member.MemberServiceImpl;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
-import org.springframework.web.bind.annotation.PatchMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import java.util.List;
 
 @RestController
 @RequiredArgsConstructor
@@ -23,9 +22,15 @@ public class MemberController {
         memberService.updatePassword(request, userDetails.getMember());
     }
 
-    @PatchMapping("/information")
-    public void updateInfo(@RequestBody InformationRequest request,
+    @PatchMapping(path = "/information", consumes = {"multipart/form-data"})
+    public void updateInfo(@ModelAttribute InformationRequest request,
                            @AuthenticationPrincipal CustomUserDetails userDetails) {
         memberService.updateInfo(request, userDetails.getMember());
     }
+
+    @GetMapping("/posts")
+    public List<PostResponse> getMemberPosts(@AuthenticationPrincipal CustomUserDetails userDetails) {
+        return memberService.getMemberPosts(userDetails.getMember());
+    }
+
 }
