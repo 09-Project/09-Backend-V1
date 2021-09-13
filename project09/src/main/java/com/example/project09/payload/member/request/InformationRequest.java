158f1ba686c11bff91e4337e36d4204bce1d421f
김범진 <sdpthf@naver.com>
2021-09-13 09:40:11 +0900

    [UPDATE] Add Profile

--- project09/src/main/java/com/example/project09/payload/member/request/InformationRequest.java
+++ project09/src/main/java/com/example/project09/payload/member/request/InformationRequest.java
@@ -2,12 +2,12 @@ package com.example.project09.payload.member.request;
 
 import lombok.AllArgsConstructor;
 import lombok.Getter;
-import lombok.NoArgsConstructor;
+import org.springframework.web.multipart.MultipartFile;
 
 @Getter
-@NoArgsConstructor
 @AllArgsConstructor
 public class InformationRequest {
     private String name;
     private String introduction;
+    private MultipartFile profile;
 }
