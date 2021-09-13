fa3b9ed9814b0955be61c5cff4f093483abd1cc0
김범진 <sdpthf@naver.com>
2021-09-13 16:39:20 +0900

    [UPDATE] FindByTitleContaining

--- project09/src/main/java/com/example/project09/entity/post/PostRepository.java
+++ project09/src/main/java/com/example/project09/entity/post/PostRepository.java
@@ -1,8 +1,10 @@
 package com.example.project09.entity.post;
 
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.List;
 
 public interface PostRepository extends JpaRepository<Post, Integer> {
     List<Post> findByMemberId(Integer id);
+    List<Post> findByTitleContaining(String keyword, Pageable pageable);
 }
