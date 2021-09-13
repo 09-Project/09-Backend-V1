e940419915e40b5913b08caae9715838f5164a0a
김범진 <sdpthf@naver.com>
2021-09-13 10:12:44 +0900

    [UPDATE] EachPostResponse

--- project09/src/main/java/com/example/project09/payload/post/response/EachPostResponse.java
+++ project09/src/main/java/com/example/project09/payload/post/response/EachPostResponse.java
@@ -1,13 +1,28 @@
 package com.example.project09.payload.post.response;
 
-import com.example.project09.entity.post.Post;
+import com.example.project09.entity.member.Member;
+import com.example.project09.entity.post.Purpose;
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import java.time.LocalDateTime;
+import java.util.List;
 
 @Getter
+@Builder
 @NoArgsConstructor
 @AllArgsConstructor
 public class EachPostResponse {
-    private Post post;
+    private String title;
+    private String content;
+    private Integer price;
+    private String transactionRegion;
+    private String openChatLink;
+    private Purpose purpose;
+    private LocalDateTime createdDate;
+    private LocalDateTime updatedDate;
+    private List<String> images;
+    private Integer likes;
+    private Member member;
 }
