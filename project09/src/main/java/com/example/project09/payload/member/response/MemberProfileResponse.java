9f2d7618c70bb42e13af14dd6122fa48d0c4f63e
김범진 <sdpthf@naver.com>
2021-09-13 10:34:18 +0900

    [UPDATE] MemberProfileResponse

---
+++ project09/src/main/java/com/example/project09/payload/member/response/MemberProfileResponse.java
@@ -0,0 +1,18 @@
+package com.example.project09.payload.member.response;
+
+import com.example.project09.payload.post.response.PostResponse;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import java.util.List;
+
+@Getter
+@Builder
+@AllArgsConstructor
+public class MemberProfileResponse {
+    private String name;
+    private String profileUrl;
+    private String introduction;
+    private List<PostResponse> posts;
+    private Integer likes;
+}
