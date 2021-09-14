06eb95da12cb67743e7496d762886e65dcd83b18
김범진 <sdpthf@naver.com>
2021-09-14 22:14:18 +0900

    [UPDATE] Image

--- project09/src/main/java/com/example/project09/entity/image/Image.java
+++ project09/src/main/java/com/example/project09/entity/image/Image.java
@@ -1,5 +1,6 @@
 package com.example.project09.entity.image;
 
+import com.example.project09.entity.BaseTimeEntity;
 import com.example.project09.entity.post.Post;
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import lombok.AllArgsConstructor;
@@ -14,19 +15,19 @@ import javax.persistence.*;
 @AllArgsConstructor
 @Entity(name = "tbl_image")
 @JsonIgnoreProperties("post")
-public class Image {
+public class Image extends BaseTimeEntity {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
-    private String profileUrl;
+    private String images;
 
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "post_id")
     private Post post;
 
-    public Image updateProfileUrl(String profileUrl) {
-        this.profileUrl = profileUrl;
+    public Image updateImages(String images) {
+        this.images = images;
         return this;
     }
 
