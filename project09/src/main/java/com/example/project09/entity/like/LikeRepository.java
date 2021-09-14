e4c3b2eb800f4819c205ec9865f8e5e89407a2f4
김범진 <sdpthf@naver.com>
2021-09-14 22:46:26 +0900

    [UPDATE] LikeRepository

--- project09/src/main/java/com/example/project09/entity/like/LikeRepository.java
+++ project09/src/main/java/com/example/project09/entity/like/LikeRepository.java
@@ -2,10 +2,12 @@ package com.example.project09.entity.like;
 
 import com.example.project09.entity.member.Member;
 import org.springframework.data.jpa.repository.JpaRepository;
+import java.util.List;
 import java.util.Optional;
 
 public interface LikeRepository extends JpaRepository<Like, Integer> {
     Optional<Like> findByMemberAndPostId(Member member, Integer postId);
+    List<Like> findByMemberId(Integer id);
     Integer countByPostId(Integer id);
     boolean existsByMemberIdAndPostId(Integer memberId, Integer postId);
 }
