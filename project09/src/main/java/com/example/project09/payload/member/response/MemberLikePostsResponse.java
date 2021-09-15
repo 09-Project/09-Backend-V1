fe7c1b2e33a595921053151153a14e8df3daa86d
김범진 <sdpthf@naver.com>
2021-09-15 13:23:22 +0900

    [UPDATE] MemberLikePostsResponse

--- project09/src/main/java/com/example/project09/payload/member/response/MemberLikePostsResponse.java
+++ project09/src/main/java/com/example/project09/payload/member/response/MemberLikePostsResponse.java
@@ -3,11 +3,12 @@ package com.example.project09.payload.member.response;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
+import java.util.List;
 
 @Getter
 @Builder
 @AllArgsConstructor
 public class MemberLikePostsResponse {
     private String title;
-    private String images;
+    private List<String> images;
 }
