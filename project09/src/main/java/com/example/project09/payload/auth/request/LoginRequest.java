aca05f4e8bcadc9f636f2978bbe8a99af0344658
김범진 <sdpthf@naver.com>
2021-09-25 20:23:01 +0900

    [UPDATE] RequestDto

--- project09/src/main/java/com/example/project09/payload/auth/request/LoginRequest.java
+++ project09/src/main/java/com/example/project09/payload/auth/request/LoginRequest.java
@@ -2,11 +2,9 @@ package com.example.project09.payload.auth.request;
 
 import lombok.AllArgsConstructor;
 import lombok.Getter;
-import lombok.NoArgsConstructor;
 import javax.validation.constraints.NotBlank;
 
 @Getter
-@NoArgsConstructor
 @AllArgsConstructor
 public class LoginRequest {
 
--- project09/src/main/java/com/example/project09/payload/auth/request/SignupRequest.java
+++ project09/src/main/java/com/example/project09/payload/auth/request/SignupRequest.java
@@ -1,6 +1,5 @@
 package com.example.project09.payload.auth.request;
 
-import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import javax.validation.constraints.NotBlank;
@@ -8,7 +7,6 @@ import javax.validation.constraints.Size;
 
 @Getter
 @NoArgsConstructor
-@AllArgsConstructor
 public class SignupRequest {
 
     @NotBlank
