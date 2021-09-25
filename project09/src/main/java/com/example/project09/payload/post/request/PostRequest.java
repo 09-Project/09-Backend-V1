80e2dbd2870fbac8360d2e8bbfd7a86bcdc203a5
김범진 <sdpthf@naver.com>
2021-09-25 21:54:56 +0900

    [ADD] Size

--- project09/src/main/java/com/example/project09/payload/post/request/PostRequest.java
+++ project09/src/main/java/com/example/project09/payload/post/request/PostRequest.java
@@ -4,15 +4,18 @@ import lombok.AllArgsConstructor;
 import lombok.Getter;
 import org.springframework.web.multipart.MultipartFile;
 import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
 
 @Getter
 @AllArgsConstructor
 public class PostRequest {
 
     @NotBlank
+    @Size(max = 40)
     private String title;
 
     @NotBlank
+    @Size(max = 500)
     private String content;
     private Integer price;
 
