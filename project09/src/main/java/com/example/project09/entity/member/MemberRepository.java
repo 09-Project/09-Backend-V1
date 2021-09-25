2850c9faf6e72b81b5145685656479bb5c281836
김범진 <sdpthf@naver.com>
2021-09-25 22:11:12 +0900

    [DELETE] ExistsBy

--- project09/src/main/java/com/example/project09/entity/member/MemberRepository.java
+++ project09/src/main/java/com/example/project09/entity/member/MemberRepository.java
@@ -4,7 +4,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.Optional;
 
 public interface MemberRepository extends JpaRepository<Member, Integer> {
-    boolean existsByNameOrUsername(String name, String username);
-    boolean existsByName(String name);
+    Optional<Member> findByName(String name);
     Optional<Member> findByUsername(String username);
 }
