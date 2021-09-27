e43c2ba3af3260c1d0fb2235606f3cc8bf5078ca
김범진 <sdpthf@naver.com>
2021-09-27 22:00:30 +0900

    [ADD] ImageServiceImpl

---
+++ project09/src/main/java/com/example/project09/service/image/ImageServiceImpl.java
@@ -0,0 +1,29 @@
+package com.example.project09.service.image;
+
+import com.example.project09.entity.image.Image;
+import com.example.project09.entity.image.ImageRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
+import javax.transaction.Transactional;
+import java.io.IOException;
+
+@Service
+@RequiredArgsConstructor
+public class ImageServiceImpl implements ImageService {
+
+    private final ImageRepository imageRepository;
+    private final S3Service s3Service;
+
+    @Override
+    @Transactional
+    public String imageUpload(MultipartFile image) throws IOException {
+        String imagePath = s3Service.upload(image, "static");
+
+        imageRepository.save(Image.builder()
+                .image(imagePath)
+                .build());
+        return imagePath;
+    }
+
+}
