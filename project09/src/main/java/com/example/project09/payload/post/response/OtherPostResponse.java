4f506226cbb55a3dc132ce77b97732e1bc1d7998
김범진 <sdpthf@naver.com>
2021-09-27 21:59:49 +0900

    [ADD] OtherPostResponse

---
+++ project09/src/main/java/com/example/project09/payload/post/response/OtherPostResponse.java
@@ -0,0 +1,11 @@
+package com.example.project09.payload.post.response;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public class OtherPostResponse {
+    private String title;
+    private String image;
+}
