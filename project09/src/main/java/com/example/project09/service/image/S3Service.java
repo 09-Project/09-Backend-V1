67604cf0a4506f5e13a1c5595ad01735bf999de7
김범진 <sdpthf@naver.com>
2021-09-29 21:21:26 +0900

    [UPDATE] S3Service

--- project09/src/main/java/com/example/project09/service/image/S3Service.java
+++ project09/src/main/java/com/example/project09/service/image/S3Service.java
@@ -2,17 +2,14 @@ package com.example.project09.service.image;
 
 import com.amazonaws.services.s3.AmazonS3Client;
 import com.amazonaws.services.s3.model.CannedAccessControlList;
+import com.amazonaws.services.s3.model.DeleteObjectRequest;
 import com.amazonaws.services.s3.model.PutObjectRequest;
-import com.example.project09.exception.FailedConvertFileException;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
-import java.io.File;
-import java.io.FileOutputStream;
 import java.io.IOException;
-import java.util.Optional;
 import java.util.UUID;
 
 @Slf4j
@@ -20,52 +17,25 @@ import java.util.UUID;
 @RequiredArgsConstructor
 public class S3Service {
 
-    private final AmazonS3Client amazonS3Client;
-
     @Value("${cloud.aws.s3.bucket}")
     public String bucket;
 
-    public String upload(MultipartFile multipartFile, String dirName) throws IOException {
-        File file = convert(multipartFile)
-                .orElseThrow(FailedConvertFileException::new);
+    private final AmazonS3Client amazonS3Client;
 
-        return uploadFile(file, dirName);
-    }
+    public String upload(MultipartFile image, String dirName) throws IOException {
+        String fileName = dirName + "/" + UUID.randomUUID() + image.getOriginalFilename();
 
-    // S3로 파일 업로드하기
-    private String uploadFile(File uploadFile, String dirName) {
-        String fileName = dirName + "/" + UUID.randomUUID() + uploadFile.getName();   // S3에 저장된 파일 이름
-        String uploadImageUrl = putS3(uploadFile, fileName); // s3로 업로드
-        removeNewFile(uploadFile);
-        return uploadImageUrl;
-    }
-
-    // S3로 업로드
-    private String putS3(File uploadFile, String fileName) {
-        amazonS3Client.putObject(new PutObjectRequest(bucket, fileName, uploadFile)
+        amazonS3Client.putObject(new PutObjectRequest(bucket, fileName, image.getInputStream(), null)
                 .withCannedAcl(CannedAccessControlList.PublicRead));
+        return fileName;
+    }
+
+    public String getFileUrl(String fileName) {
         return amazonS3Client.getUrl(bucket, fileName).toString();
     }
 
-    // 로컬에 저장된 파일 지우기
-    private void removeNewFile(File targetFile) {
-        if (targetFile.delete()) {
-            log.info("File delete success");
-            return;
-        }
-        log.info("File delete fail");
+    public void removeFile(String imagePath) {
+        amazonS3Client.deleteObject(new DeleteObjectRequest(bucket, imagePath));
     }
 
-    // 로컬에 파일 업로드 하기
-    private Optional<File> convert(MultipartFile file) throws IOException {
-        File convertFile = new File(System.getProperty("user.dir") + "/" + file.getOriginalFilename());
-        if (convertFile.createNewFile()) { // 바로 위에서 지정한 경로에 File이 생성됨 (경로가 잘못되었다면 생성 불가능)
-            try (FileOutputStream fos = new FileOutputStream(convertFile)) { // FileOutputStream 데이터를 파일에 바이트 스트림으로 저장하기 위함
-                fos.write(file.getBytes());
-            }
-            return Optional.of(convertFile);
-        }
-
-        return Optional.empty();
-    }
 }
