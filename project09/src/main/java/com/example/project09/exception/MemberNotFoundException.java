c810f0fb978af32dc385b984e84c97839f9720ea
김범진 <sdpthf@naver.com>
2021-10-02 18:13:52 +0900

    [UPDATE] MemberNotFoundException

--- project09/src/main/java/com/example/project09/exception/MemberNotFoundException.java
+++ project09/src/main/java/com/example/project09/exception/MemberNotFoundException.java
@@ -3,8 +3,8 @@ package com.example.project09.exception;
 import com.example.project09.error.ErrorCode;
 import com.example.project09.error.exception.BusinessException;
 
-public class UserNotFoundException extends BusinessException {
-    public UserNotFoundException() {
+public class MemberNotFoundException extends BusinessException {
+    public MemberNotFoundException() {
         super(ErrorCode.USER_NOT_FOUND);
     }
 }
