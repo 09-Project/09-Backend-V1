a942b3eae9abb2f2ecb2d95a1381ba620792e158
김범진 <sdpthf@naver.com>
2021-10-03 17:46:10 +0900

    [UPDATE] Schema Signup Request

--- project09/src/main/java/com/example/project09/payload/auth/request/SignupRequest.java
+++ project09/src/main/java/com/example/project09/payload/auth/request/SignupRequest.java
@@ -1,5 +1,6 @@
 package com.example.project09.payload.auth.request;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import javax.validation.constraints.NotBlank;
@@ -7,10 +8,12 @@ import javax.validation.constraints.Size;
 
 @Getter
 @NoArgsConstructor
+@Schema(description = "회원 가입")
 public class SignupRequest {
 
     @NotBlank
     @Size(max = 10)
+    @Schema(description = "닉네임", maxLength = 10)
     private String name;
 
     @NotBlank
