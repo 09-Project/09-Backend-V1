9e8ee153dc23528385b3f2bfd1046366abaa558c
김범진 <sdpthf@naver.com>
2021-10-03 17:45:39 +0900

    [ADD] Handle MethodArgumentNotValidException

--- project09/src/main/java/com/example/project09/error/GlobalExceptionHandler.java
+++ project09/src/main/java/com/example/project09/error/GlobalExceptionHandler.java
@@ -3,6 +3,7 @@ package com.example.project09.error;
 import com.example.project09.error.exception.BusinessException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
@@ -14,6 +15,11 @@ public class GlobalExceptionHandler {
         final ErrorCode errorCode = e.getErrorCode();
         return new ResponseEntity<>(new ErrorResponse(errorCode.getMessage()),
                 HttpStatus.valueOf(errorCode.getStatus()));
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<ErrorResponse> methodValidException(MethodArgumentNotValidException e) {
+        return new ResponseEntity<>(new ErrorResponse("Max Value"), HttpStatus.BAD_REQUEST);
     }
 
 }
