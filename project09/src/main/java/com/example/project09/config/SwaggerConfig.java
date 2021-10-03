bbd571dca43a9f445d5cacab6c418d7cff14cad3
김범진 <sdpthf@naver.com>
2021-10-03 11:41:43 +0900

    [UPDATE] SwaggerConfig

--- project09/src/main/java/com/example/project09/config/SwaggerConfig.java
+++ project09/src/main/java/com/example/project09/config/SwaggerConfig.java
@@ -8,22 +8,41 @@ import springfox.documentation.builders.RequestHandlerSelectors;
 import springfox.documentation.service.ApiInfo;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+import java.util.HashSet;
+import java.util.Set;
 
 @Configuration
+@EnableSwagger2
 public class SwaggerConfig {
 
     @Bean
     public Docket api() {
         return new Docket(DocumentationType.SWAGGER_2)
+                .consumes(getConsumeContentTypes())
+                .produces(getProduceContentTypes())
                 .useDefaultResponseMessages(false)
+                .apiInfo(getApiInfo())
                 .select()
                 .apis(RequestHandlerSelectors.basePackage("com.example.project09.controller"))
                 .paths(PathSelectors.any())
-                .build()
-                .apiInfo(apiInfo());
+                .build();
     }
 
-    private ApiInfo apiInfo() {
+    private Set<String> getConsumeContentTypes() {
+        Set<String> consumes = new HashSet<>();
+        consumes.add("application/json;charset=UTF-8");
+        consumes.add("application/form-data");
+        return consumes;
+    }
+
+    private Set<String> getProduceContentTypes() {
+        Set<String> produces = new HashSet<>();
+        produces.add("application/json;charset=UTF-8");
+        return produces;
+    }
+
+    private ApiInfo getApiInfo() {
         return new ApiInfoBuilder()
                 .title("PROJECT09 API")
                 .description("공동구매 API 명세서입니다.")
