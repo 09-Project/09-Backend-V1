3048ec6c62c4c2b8f20f17936510bdccb560a88c
김범진 <sdpthf@naver.com>
2021-10-04 13:37:34 +0900

    [UPDATE] OpenApiConfig

--- project09/src/main/java/com/example/project09/config/OpenApiConfig.java
+++ project09/src/main/java/com/example/project09/config/OpenApiConfig.java
@@ -5,15 +5,21 @@ import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Contact;
 import io.swagger.v3.oas.models.info.Info;
 import io.swagger.v3.oas.models.info.License;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
+import io.swagger.v3.oas.models.servers.Server;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import java.util.Arrays;
+import java.util.List;
 
 @Configuration
 public class OpenApiConfig {
 
     @Bean
-    public OpenAPI openAPI(@Value("${springdoc.version}") String appVersion) {
+    public OpenAPI openAPI(@Value("${springdoc.version}") String appVersion,
+                           @Value("${springdoc.url}") String url) {
         Info info = new Info()
                 .title("Project09 API")
                 .version(appVersion)
@@ -21,9 +27,19 @@ public class OpenApiConfig {
                 .contact(new Contact().name("softpeanut").url("https://github.com/09-Project/09_Backend").email("sdpthf@naver.com"))
                 .license(new License().name("Apache License Version 2.0").url("http://www.apache.org/licenses/LICENSE-2.0"));
 
+        List<Server> servers = Arrays.asList(new Server().url(url));
+
+        SecurityScheme securityScheme = new SecurityScheme()
+                .type(SecurityScheme.Type.HTTP).scheme("bearer").bearerFormat("JWT")
+                .in(SecurityScheme.In.HEADER).name("Authorization");
+
+        SecurityRequirement schemaRequirement = new SecurityRequirement().addList("bearerAuth");
+
         return new OpenAPI()
-                .components(new Components())
-                .info(info);
+                .components(new Components().addSecuritySchemes("bearerAuth", securityScheme))
+                .security(Arrays.asList(schemaRequirement))
+                .info(info)
+                .servers(servers);
     }
 
 }
