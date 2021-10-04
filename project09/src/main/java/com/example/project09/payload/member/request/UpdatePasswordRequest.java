8cc9c76e3238b1f359f3d8e09ece474145000be2
김범진 <sdpthf@naver.com>
2021-10-04 16:24:24 +0900

    [UPDATE] Password Request -> SnakeCase

--- project09/src/main/java/com/example/project09/payload/member/request/UpdatePasswordRequest.java
+++ project09/src/main/java/com/example/project09/payload/member/request/UpdatePasswordRequest.java
@@ -1,11 +1,14 @@
 package com.example.project09.payload.member.request;
 
+import com.fasterxml.jackson.databind.PropertyNamingStrategy;
+import com.fasterxml.jackson.databind.annotation.JsonNaming;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import javax.validation.constraints.NotBlank;
 
 @Getter
 @NoArgsConstructor
+@JsonNaming(value = PropertyNamingStrategy.SnakeCaseStrategy.class)
 public class UpdatePasswordRequest {
 
     @NotBlank
