beff1c124b2233b8339d62b877acce15f0612c5d
김범진 <sdpthf@naver.com>
2021-10-04 15:10:44 +0900

    [UPDATE] Valid UpdateInfo Request

--- project09/src/main/java/com/example/project09/payload/member/request/UpdateInformationRequest.java
+++ project09/src/main/java/com/example/project09/payload/member/request/UpdateInformationRequest.java
@@ -4,17 +4,22 @@ import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import org.springframework.web.multipart.MultipartFile;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
 
 @Getter
 @NoArgsConstructor
 public class UpdateInformationRequest {
 
+    @NotBlank
     @Size(max = 10)
     @Schema(description = "닉네임", maxLength = 10)
     private String name;
 
     @Size(max = 200)
     private String introduction;
+
+    @NotNull
     private MultipartFile profile;
 }
