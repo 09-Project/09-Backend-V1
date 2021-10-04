0d547c60cab3054bf5322f7d08b5c9e9e5705683
김범진 <sdpthf@naver.com>
2021-10-04 14:07:28 +0900

    [UPDATE] MemberFacade

--- project09/src/main/java/com/example/project09/facade/MemberFacade.java
+++ project09/src/main/java/com/example/project09/facade/MemberFacade.java
@@ -11,7 +11,7 @@ public class MemberFacade {
     public static Member getMember() {
         Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
 
-        if(authentication == null || authentication.getPrincipal() == null )
+        if(authentication == null || authentication.getPrincipal() == null)
             throw new MemberNotFoundException();
 
         return (Member) authentication.getPrincipal();
