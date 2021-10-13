ccb03f7d8d3ad731e18a7f462bbbf5884aad7984
김범진 <sdpthf@naver.com>
2021-10-13 20:49:55 +0900

    [UPDATE] Contact Url

--- project09/src/main/java/com/example/project09/config/OpenApiConfig.java
+++ project09/src/main/java/com/example/project09/config/OpenApiConfig.java
@@ -24,7 +24,7 @@ public class OpenApiConfig {
                 .title("Project09 API")
                 .version(appVersion)
                 .description("Project09 API 명세서입니다.")
-                .contact(new Contact().name("softpeanut").url("https://github.com/09-Project/09_Backend").email("sdpthf@gmail.com"))
+                .contact(new Contact().name("softpeanut").url("https://github.com").email("sdpthf@gmail.com"))
                 .license(new License().name("Apache License Version 2.0").url("http://www.apache.org/licenses/LICENSE-2.0"));
 
         List<Server> servers = Arrays.asList(new Server().url(url));
