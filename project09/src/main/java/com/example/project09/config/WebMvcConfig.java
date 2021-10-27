659a7203d7dea47c72c6744038fdf4601388ca3e
김범진 <sdpthf@naver.com>
2021-10-27 10:01:16 +0900

    [UPDATE] Cors Config

--- project09/src/main/java/com/example/project09/config/WebMvcConfig.java
+++ project09/src/main/java/com/example/project09/config/WebMvcConfig.java
@@ -10,8 +10,8 @@ public class WebMvcConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://localhost:3000")
-                .allowedMethods("OPTIONS", "GET", "POST", "PUT", "PATCH", "DELETE");
+                .allowedMethods("GET", "POST", "PUT", "PATCH", "DELETE")
+                .allowedOrigins("http://localhost:3000");
     }
 
 }
