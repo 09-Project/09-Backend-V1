e39d0d21fb89bf7931e3447ab8ed04e505df6268
김범진 <sdpthf@naver.com>
2021-10-28 18:49:30 +0900

    [UPDATE] Allowed port 3001

--- project09/src/main/java/com/example/project09/config/WebMvcConfig.java
+++ project09/src/main/java/com/example/project09/config/WebMvcConfig.java
@@ -11,7 +11,7 @@ public class WebMvcConfig implements WebMvcConfigurer {
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
                 .allowedMethods("GET", "POST", "PUT", "PATCH", "DELETE")
-                .allowedOrigins("http://localhost:3000");
+                .allowedOrigins("http://localhost:3000", "http://localhost:3001");
     }
 
 }
