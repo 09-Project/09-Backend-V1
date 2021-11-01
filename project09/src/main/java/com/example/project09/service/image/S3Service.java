f9ee867699ab21b6e2eb9b2edf2d8c3e75d48b57
김범진 <sdpthf@naver.com>
2021-11-01 16:06:04 +0900

    [UPDATE] Download File Service

--- project09/src/main/java/com/example/project09/service/image/S3Service.java
+++ project09/src/main/java/com/example/project09/service/image/S3Service.java
@@ -1,16 +1,20 @@
 package com.example.project09.service.image;
 
 import com.amazonaws.services.s3.AmazonS3Client;
-import com.amazonaws.services.s3.model.CannedAccessControlList;
-import com.amazonaws.services.s3.model.DeleteObjectRequest;
-import com.amazonaws.services.s3.model.PutObjectRequest;
-import com.example.project09.exception.S3ConnectionFailedException;
+import com.amazonaws.services.s3.model.*;
+import com.amazonaws.util.IOUtils;
 import com.example.project09.config.S3Config;
+import com.example.project09.exception.S3ConnectionFailedException;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 import java.io.IOException;
+import java.net.URLEncoder;
 import java.util.UUID;
 
 @Slf4j
@@ -31,6 +35,26 @@ public class S3Service {
         }
 
         return fileName;
+    }
+
+    public ResponseEntity<byte[]> download(String storedFileName) {
+        S3Object object = amazonS3Client.getObject(new GetObjectRequest(s3Config.getBucket(), storedFileName));
+        S3ObjectInputStream oi = object.getObjectContent();
+
+        try {
+            byte[] bytes = IOUtils.toByteArray(oi);
+
+            String fileName = URLEncoder.encode(storedFileName, "UTF-8").replaceAll("\\+", "%20");
+
+            HttpHeaders httpHeaders = new HttpHeaders();
+            httpHeaders.setContentType(MediaType.APPLICATION_OCTET_STREAM);
+            httpHeaders.setContentLength(bytes.length);
+            httpHeaders.setContentDispositionFormData("attachment", fileName);
+
+            return new ResponseEntity<>(bytes, httpHeaders, HttpStatus.OK);
+        } catch (IOException e) {
+            throw new S3ConnectionFailedException();
+        }
     }
 
     public String getFileUrl(String fileName) {
