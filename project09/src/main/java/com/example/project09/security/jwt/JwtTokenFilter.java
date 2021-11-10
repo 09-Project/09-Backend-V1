3dce15545d0af99e553d59b5ca05309b2cdab204
김범진 <sdpthf@naver.com>
2021-11-10 14:45:33 +0900

    ⚡️ :: @Component 제거

--- project09/src/main/java/com/example/project09/security/jwt/JwtTokenFilter.java
+++ project09/src/main/java/com/example/project09/security/jwt/JwtTokenFilter.java
@@ -3,7 +3,6 @@ package com.example.project09.security.jwt;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
-import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
 import javax.servlet.FilterChain;
 import javax.servlet.ServletException;
@@ -11,7 +10,6 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
-@Component
 @RequiredArgsConstructor
 public class JwtTokenFilter extends OncePerRequestFilter {
 
