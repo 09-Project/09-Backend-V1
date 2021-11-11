22cccaf2f1557b0110d35ab40e18f7d52a06f10f
김범진 <sdpthf@naver.com>
2021-11-11 23:21:25 +0900

    ⚡️ :: NPE 방지

--- project09/src/main/java/com/example/project09/facade/MemberFacade.java
+++ project09/src/main/java/com/example/project09/facade/MemberFacade.java
@@ -1,6 +1,8 @@
 package com.example.project09.facade;
 
 import com.example.project09.entity.member.Member;
+import com.example.project09.exception.InvalidTokenException;
+import com.example.project09.exception.MemberNotFoundException;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 
@@ -11,7 +13,7 @@ public class MemberFacade {
 
         if(authentication == null || authentication.getPrincipal() == null
                 || !(authentication.getPrincipal() instanceof Member))
-            return null;
+            throw new InvalidTokenException();
 
         return (Member) authentication.getPrincipal();
     }
